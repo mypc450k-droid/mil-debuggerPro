b841a814558071dc35b86aeacc62f98cd6c26045
mypc450k-droid <mypc450k@gmail.com>
2026-09-19 22:59:20 +0530

    test: add demo model for local validation

--- test_models/create_demo_model.m
+++ test_models/create_demo_model.m
@@ -1,5 +1,5 @@
 function modelName=create_demo_model()
-%CREATE_DEMO_MODEL Create a small MIL Debugger test model.
+%CREATE_DEMO_MODEL Create a small model for MIL Debugger Pro validation.
 modelName='MILDebuggerDemo';
 if bdIsLoaded(modelName), close_system(modelName,0); end
 new_system(modelName); open_system(modelName);
