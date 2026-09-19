f89b0d79a325c309a02f9b6dd210c57d9acce11c
mypc450k-droid <mypc450k@gmail.com>
2026-09-19 22:56:21 +0530

    test: add demo model docs and CI structure

---
+++ test_models/create_demo_model.m
@@ -0,0 +1,15 @@
+function modelName=create_demo_model()
+%CREATE_DEMO_MODEL Create a small MIL Debugger test model.
+modelName='MILDebuggerDemo';
+if bdIsLoaded(modelName), close_system(modelName,0); end
+new_system(modelName); open_system(modelName);
+add_block('simulink/Sources/Sine Wave',[modelName '/Input']);
+add_block('simulink/Math Operations/Gain',[modelName '/Gain'],'Gain','2');
+add_block('simulink/Math Operations/Sum',[modelName '/Sum']);
+add_block('simulink/Sinks/Scope',[modelName '/Scope']);
+add_line(modelName,'Input/1','Gain/1');
+add_line(modelName,'Gain/1','Sum/1');
+add_line(modelName,'Gain/1','Sum/2');
+add_line(modelName,'Sum/1','Scope/1');
+set_param(modelName,'StopTime','1');
+end
